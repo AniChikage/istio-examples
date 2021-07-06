d79aaf0508f0af1a99b72ccfc9075c78f95234bc
hanzhis <hanzhis@cn.ibm.com>
2021-07-05 22:44:54 -0400

    add comments

--- projects/comments/app.py
+++ projects/comments/app.py
@@ -1,15 +1,15 @@
 from flask import Flask,jsonify,request
-import config
+import config, loader
 
 app = Flask(__name__)
 
-@app.route('/{}/ratings'.format(config.VERSION),methods=['GET'])
-def comments():
-    name = 'sfs'
-    if 'name' in request.args:
-        name = request.args['name']
-    data={'data': 'hello ' + name}
-    return jsonify(data)
+@app.route("/comments/<path:book_id>",methods=["GET"])
+def comments(book_id):
+    data_loader = loader.Loader()
+    data = data_loader.load()
+    if book_id not in data.keys():
+        return json.dumps({})
+    return data[book_id]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
